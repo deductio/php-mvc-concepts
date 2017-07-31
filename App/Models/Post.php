f9a667e6fb2697462e409df708cd28d6317401c0
alex <alex.shustrov@gmail.com>
2017-08-01 01:59:05 +0300

    adding data base connection config

--- App/Models/Post.php
+++ App/Models/Post.php
@@ -18,13 +18,10 @@ class Post extends \Core\Model
      * @return array
      */
     public static function getAll()
-    {    
+    {
         try {
-
             $db = static::getDB();
-
-            $stmt = $db->query('SELECT id, title, content FROM posts 
-                                ORDER BY created_at');
+            $stmt = $db->query('SELECT id, title, content FROM posts ORDER BY created_at');
             $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
             return $results;
