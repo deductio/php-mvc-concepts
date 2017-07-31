0b3d43dc998716e9f504bd64bc02ed7c2476393c
alex <alex.shustrov@gmail.com>
2017-08-01 00:17:50 +0300

    composer + autoloader

--- public/index.php
+++ public/index.php
@@ -6,24 +6,22 @@
  * PHP version 5.4
  */
 
-/**
- * Twig
- */
-require_once dirname(__DIR__) . '/../vendor/Twig/lib/Twig/Autoloader.php';
-Twig_Autoloader::register();
-
 
 /**
- * Autoloader
+ * Composer
  */
-spl_autoload_register(function ($class) {
-    $root = dirname(__DIR__);   // get the parent directory
-    $file = $root . '/' . str_replace('\\', '/', $class) . '.php';
-    if (is_readable($file)) {
-        require $root . '/' . str_replace('\\', '/', $class) . '.php';
-    }
-});
+require '../vendor/autoload.php';
 
+///**
+// * Autoloader
+// */
+//spl_autoload_register(function ($class) {
+//    $root = dirname(__DIR__);   // get the parent directory
+//    $file = $root . '/' . str_replace('\\', '/', $class) . '.php';
+//    if (is_readable($file)) {
+//        require $root . '/' . str_replace('\\', '/', $class) . '.php';
+//    }
+//});
 
 /**
  * Routing
