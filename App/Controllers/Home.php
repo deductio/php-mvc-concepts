6a88dc22c23a37b3563c39cffc2d037b17146cf6
alex <alex.shustrov@gmail.com>
2017-08-04 12:22:29 +0300

    jquery, bootstrap added

--- App/Controllers/Home.php
+++ App/Controllers/Home.php
@@ -40,14 +40,8 @@ class Home extends \Core\Controller
      */
     public function indexAction()
     {
-        /*
-        View::render('Home/index.php', [
-            'name'    => 'Dave',
-            'colours' => ['red', 'green', 'blue']
-        ]);
-        */
         View::renderTemplate('Home/index.html', [
-            'name'    => 'Dave',
+            'name'    => 'alex',
             'colours' => ['red', 'green', 'blue']
         ]);
     }
